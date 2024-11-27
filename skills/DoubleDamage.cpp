37288e09dd608ca3c138d57e7895e9dbbb042422
Alexander-Snigirev <144445249+Alexander-Snigirev@users.noreply.github.com>
2024-11-27 23:47:41 +0300

    Delete skills directory

--- skills/DoubleDamage.cpp
+++
@@ -1,7 +0,0 @@
-#include "DoubleDamage.h"
-
-void DoubleDamage::apply(GameField& field, ShipManager& shipManager, int x, int y)
-{
-    field.attack(x,y,shipManager);
-    field.attack(x,y,shipManager);
-}
