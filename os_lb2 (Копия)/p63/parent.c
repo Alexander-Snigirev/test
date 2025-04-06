32312a93116a8ba238e093b9a8a98994ce20a12b
Alexander-Snigirev <144445249+Alexander-Snigirev@users.noreply.github.com>
2025-04-06 16:36:13 +0300

    g

--- os_lb2 (Копия)/p63/parent.c
+++ os_lb2 (Копия)/p63/parent.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <bits/types/sigset_t.h>
+#include <bits/sigaction.h>
 
 void signal_handler(int sig) {
     printf("PID %d: Получен сигнал %d\n", getpid(), sig);
@@ -10,7 +12,16 @@ void signal_handler(int sig) {
 
 int main() {
     pid_t pid;
+    sigset_t set;
+    int sig;
 
+    // Блокируем SIGUSR1 и SIGUSR2 для использования в sigwait
+    sigemptyset(&set);
+    sigaddset(&set, SIGUSR1);
+    sigaddset(&set, SIGUSR2);
+    sigprocmask(SIG_BLOCK, &set, NULL);
+
+    // Устанавливаем обработчик для SIGUSR1
     signal(SIGUSR1, signal_handler);
     printf("Родитель (PID %d): Установлен обработчик для SIGUSR1\n", getpid());
 
@@ -19,18 +30,28 @@ int main() {
         perror("Ошибка fork");
         exit(1);
     }
-    if (pid == 0) { 
+
+    if (pid == 0) { // Дочерний процесс
         printf("Потомок (PID %d): После fork\n", getpid());
+        
+        // Проверяем обработчик
         kill(getpid(), SIGUSR1);
-        sleep(1); 
-        execl("./child_signal", "child_signal", (char*)NULL);
+
+        // Отправляем родителю сигнал SIGUSR2, что готовы к exec
+        kill(getppid(), SIGUSR2);
+
+        // Вызываем exec
+        execl("./child", "child", (char*)NULL);
         perror("Ошибка exec");
         exit(1);
-    } else { 
-        sleep(2); 
-        printf("Родитель (PID %d): Отправляю SIGUSR1 потомку\n", getpid());
-        kill(pid, SIGUSR1);
-        wait(NULL);
+    } else { // Родительский процесс
+        // Ждём SIGUSR2 от потомка
+        sigwait(&set, &sig);
+        if (sig == SIGUSR2) {
+            printf("Родитель (PID %d): Получен SIGUSR2 от потомка, отправляю SIGUSR1\n", getpid());
+            kill(pid, SIGUSR1);
+            wait(NULL);
+        }
     }
 
     return 0;
